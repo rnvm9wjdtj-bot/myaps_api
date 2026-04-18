d60bc85539c0185987b4839089d1a9e68011928b
超哥 <2982212683@qq.com>
2026-04-18 09:22:03 +0800

    解决冲突

--- scripts/migrate/monitor_orm_config.py
+++ scripts/migrate/monitor_orm_config.py
@@ -1,9 +1,9 @@
-from core import TORTOISE_ORM_CONFIG
+from core import TORTOISE_ORM_CONFIG, SQLITE_FILE
 
-# 复用 TORTOISE_ORM_CONFIG 中的 local_data 连接配置和 monitor_models 应用配置
+# 复用 TORTOISE_ORM_CONFIG 中的 SQLITE_FILE 连接配置和 monitor_models 应用配置
 monitor_orm_config = {
     "connections": {
-        "local_data": TORTOISE_ORM_CONFIG["connections"]["local_data"]
+        SQLITE_FILE: TORTOISE_ORM_CONFIG["connections"][SQLITE_FILE]
     },
     "apps": {
         "monitor_models": TORTOISE_ORM_CONFIG["apps"]["monitor_models"]
